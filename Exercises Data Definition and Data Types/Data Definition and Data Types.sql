ce9f493cfca98a3889dcc4df8931ba7e37f57137
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-01-17 19:42:41 +0200

    added 12-17 exercises

--- Exercises Data Definition and Data Types/Data Definition and Data Types.sql
+++ Exercises Data Definition and Data Types/Data Definition and Data Types.sql
@@ -164,3 +164,178 @@ INSERT INTO movies (title) VALUES
 ('test'),
 ('test'),
 ('test');
+
+-- 12.	Car Rental Database
+CREATE DATABASE car_rental;
+USE car_rental;
+
+CREATE TABLE categories(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    category VARCHAR(50) NOT NULL,
+    daily_rate DOUBLE(2,2),
+    weekly_rate DOUBLE(2,2),
+    monthly_rate DOUBLE(2,2),
+    weekend_rate DOUBLE(2,2)
+);
+
+INSERT INTO categories (category) VALUES 
+('test'),
+('test'),
+('test');
+
+CREATE TABLE cars(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    plate_number VARCHAR(10) NOT NULL,
+    make VARCHAR(30),
+    model VARCHAR(20),
+    car_year YEAR,
+    category_id INT,
+    doors CHAR,
+    picture BLOB,
+    car_condition VARCHAR(255),
+    available BOOLEAN
+);
+
+INSERT INTO cars (plate_number) VALUES
+('1'),
+('2'),
+('3');
+
+CREATE TABLE employees (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    title VARCHAR(20),
+    notes VARCHAR(255)
+);
+
+INSERT INTO employees (first_name, last_name) VALUES
+('test', 'testov'),
+('test2', 'testov2'),
+('test3', 'testov3');
+
+CREATE TABLE customers (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    driver_license_number INT NOT NULL,
+    full_name VARCHAR(50),
+	address VARCHAR(50),
+    city VARCHAR(20),
+    zip_code CHAR,
+    notes VARCHAR(255)
+);
+
+INSERT INTO customers (driver_license_number) VALUES
+('1234'),
+('1235'),
+('1236');
+
+
+CREATE TABLE rental_orders (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    employee_id INT NOT NULL,
+    customer_id INT NOT NULL,
+    car_id INT NOT NULL,
+    car_condition VARCHAR(255),
+    tank_level INT,
+    kilometrage_start INT,
+    kilometrage_end INT,
+    total_kilometrage INT,
+    start_date DATETIME,
+    end_date DATETIME,
+    total_days INT,
+    rate_applied INT,
+    tax_rate INT,
+    order_status INT,
+    notes VARCHAR(255)
+);
+
+INSERT INTO rental_orders (employee_id, customer_id, car_id) VALUES
+('1','2','3'),
+('4','5','6'),
+('7','8','9');
+
+-- 13.	Basic Insert
+CREATE DATABASE soft_uni;
+USE soft_uni;
+
+CREATE TABLE towns (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO towns (name) VALUES
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas');
+
+CREATE TABLE addresses (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    address_text VARCHAR(50),
+    town_id INT,
+    FOREIGN KEY (town_id)
+		REFERENCES towns (id)
+);
+
+CREATE TABLE departments (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL
+);
+
+INSERT INTO departments (name) VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance');
+
+CREATE TABLE employees (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    middle_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    job_title VARCHAR(20) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+		REFERENCES departments (id),
+	hire_date DATETIME,
+    salary DOUBLE(5,2),
+    address_id INT,
+    FOREIGN KEY (address_id)
+		REFERENCES addresses (id)
+);
+
+
+INSERT INTO employees (first_name, middle_name, last_name, job_title, department_id, hire_date, salary) VALUES
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer','4', '2013-02-01', '3500.00'),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer','1', '2004-03-02', '4000.00'),
+('Maria', 'Petrova', 'Ivanova', 'Intern','5', '2016-08-28', '525.25'),
+('Georgi', 'Terziev', 'Ivanov', 'CEO','2', '2007-12-09', '3000.00'),
+('Peter', 'Pan', 'Pan', 'Intern','3', '2016-08-28', '599.88');
+
+-- 14.	Basic Select All Fields
+SELECT * FROM towns;
+SELECT * FROM departments;
+SELECT * FROM employees;
+
+-- 15.	Basic Select All Fields and Order Them
+SELECT * FROM towns 
+ORDER BY name ASC;
+SELECT * FROM departments  
+ORDER BY name ASC;
+SELECT * FROM employees 
+ORDER BY salary DESC;
+
+-- 16.	Basic Select Some Fields
+SELECT name FROM towns
+ORDER BY name ASC;
+SELECT name FROM departments
+ORDER BY name ASC;
+SELECT first_name, last_name, job_title, salary FROM employees
+ORDER BY salary DESC;
+
+-- 17.	Increase Employees Salary
+UPDATE employees SET salary = salary * 1.1;
+SELECT salary FROM employees;
+
+
