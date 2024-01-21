151e3191824e085474db682b14137e70a9e0078d
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-01-21 23:47:07 +0200

    added rest of the exercises

--- Exercises Data Aggregation/Data Aggregation.sql
+++ Exercises Data Aggregation/Data Aggregation.sql
@@ -39,27 +39,92 @@ GROUP BY deposit_group, magic_wand_creator
 ORDER BY magic_wand_creator, deposit_group ;
 
 -- 9. Age Groups
-SELECT age FROM wizzard_deposits wd ;
-
+SELECT CASE
+	WHEN age BETWEEN 0 AND 10 THEN  '[0-10]'
+	WHEN age BETWEEN 11 AND 20 THEN  '[11-20]'
+	WHEN age BETWEEN 21 AND 30 THEN  '[21-30]'
+	WHEN age BETWEEN 31 AND 40 THEN  '[31-40]'
+	WHEN age BETWEEN 41 AND 50 THEN  '[41-50]'
+	WHEN age BETWEEN 51 AND 60 THEN  '[51-60]'
+	WHEN age >= 61 THEN  '[61+]'
+END AS 'age_group', count(first_name) AS 'wizard_count' FROM wizzard_deposits wd 
+GROUP BY `age_group`
+ORDER BY `age_group`;
 
 -- 10. First Letter
-SELECT DISTINCT LEFT(first_name,1) FROM wizzard_deposits wd 
+SELECT LEFT(first_name,1) AS 'first_letter' FROM wizzard_deposits wd 
 WHERE wd.deposit_group = 'Troll Chest'
-ORDER BY LEFT(first_name,1);
+GROUP BY `first_letter`
+ORDER BY `first_letter`;
 
 -- 11. Average Interest 
-
+SELECT deposit_group, is_deposit_expired  , AVG(deposit_interest) AS 'average_interest'  FROM wizzard_deposits wd 
+WHERE deposit_start_date > '1985-01-01'
+GROUP BY deposit_group , is_deposit_expired
+ORDER BY deposit_group DESC, is_deposit_expired;
 
 -- 12. Employees Minimum Salaries
+SELECT department_id, min(salary) AS 'minimum_salary'  FROM employees e 
+WHERE hire_date > '2000-01-01'
+GROUP BY department_id 
+HAVING department_id IN (2,5,7)
+ORDER BY department_id ;
 
 -- 13. Employees Average Salaries
+CREATE TABLE salary_more_than_3000 AS
+SELECT * FROM employees e 
+WHERE salary > 30000;
+
+DELETE FROM salary_more_than_3000 
+WHERE manager_id = 42;
+
+UPDATE salary_more_than_3000 
+SET salary = salary + 5000
+WHERE department_id = 1;
+
+SELECT department_id, AVG(salary) AS 'avg_salary' FROM salary_more_than_3000 smt 
+GROUP BY department_id 
+ORDER BY department_id ASC ;
 
 -- 14. Employees Maximum Salaries
+SELECT department_id, MAX(salary) AS 'max_salary' FROM employees e 
+GROUP BY department_id 
+HAVING `max_salary` NOT BETWEEN 30000 AND 70000
+ORDER BY department_id ;
 
 -- 15. Employees Count Salaries
+SELECT count(salary) FROM employees e 
+WHERE manager_id IS NULL;
 
 -- 16. 3rd Highest Salary*
+SELECT DISTINCT 
+	department_id,
+	(
+	SELECT DISTINCT
+		salary
+	FROM
+		employees e1
+	WHERE
+		e1.department_id = employees.department_id
+	ORDER BY
+		salary DESC
+	LIMIT 2,1) AS 'third_highest_salary'
+FROM
+	employees
+HAVING third_highest_salary IS NOT NULL 
+ORDER BY
+	department_id;
 
 -- 17. Salary Challenge**
+SELECT first_name, last_name, department_id  FROM employees e 
+WHERE salary > (
+	SELECT avg(salary) FROM employees e2
+	WHERE e.department_id = e2.department_id
+	)
+ORDER BY department_id, employee_id
+LIMIT 10;
 
 -- 18. Departments Total Salaries
+SELECT department_id, SUM(salary) AS 'total_salary'  FROM employees e 
+GROUP BY department_id 
+ORDER BY department_id ;
