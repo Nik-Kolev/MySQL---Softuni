a3a788da69ea92ae08b2f12a412fbd6b85378e90
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-01-27 23:44:43 +0200

    added exercises 6-8

--- Exercises Database Programmability/Database Programmability.sql
+++ Exercises Database Programmability/Database Programmability.sql
@@ -70,3 +70,40 @@ RETURN (
 delimiter ;
 
 SELECT ufn_get_salary_level(44440);
+
+-- 6.	Employees by Salary Level
+delimiter $$
+CREATE PROCEDURE usp_get_employees_by_salary_level(salary_param varchar(7))
+BEGIN
+	SELECT first_name, last_name  FROM employees e 
+	WHERE salary_param = 'High' AND  salary > 50000 OR 
+			 salary_param = 'Low' AND  salary < 30000 OR 
+			 salary_param = 'Average' AND  salary >= 30000 AND salary <= 50000
+	ORDER BY first_name DESC , last_name DESC;
+END	$$
+delimiter ;
+	
+CALL usp_get_employees_by_salary_level('high');
+
+-- 7.	Define Function
+delimiter $$
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters varchar(50), word varchar(50))
+RETURNS int
+RETURN word REGEXP (CONCAT('^[', set_of_letters, ']+$'));
+delimiter ;
+
+SELECT ufn_is_word_comprised('Nik', 'Niko');
+	
+-- 8.	Find Full Name
+delimiter $$
+CREATE PROCEDURE usp_get_holders_full_name()
+BEGIN	
+	SELECT concat(first_name, ' ', last_name) AS full_name FROM account_holders ah 
+	ORDER BY full_name, id;
+END $$
+
+delimiter ;
+
+CALL usp_get_holders_full_name();
+
+
