872e8189ca602d4bcd6f6764b27ac88aca5ba322
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-01-28 20:42:47 +0200

    added last exercise

--- Exercises Subqueries and JOINs/Subqueries and JOINs.sql
+++ Exercises Subqueries and JOINs/Subqueries and JOINs.sql
@@ -152,3 +152,21 @@ LEFT JOIN mountains_countries mc ON mc.country_code =  c.country_code
 WHERE mc.mountain_id IS NULL;
 
 -- 17.  Highest Peak and Longest River by Country
+SELECT
+    c.country_name,
+    MAX(p.elevation) AS 'highest_peak_elevation',
+    MAX(r.length) AS 'longest_river_length'
+FROM
+    countries c
+    LEFT JOIN mountains_countries mc USING (country_code)
+    LEFT JOIN peaks p USING (mountain_id)
+    LEFT JOIN countries_rivers cr USING (country_code)
+    LEFT JOIN rivers r ON cr.river_id = r.id
+GROUP BY
+    c.country_name
+ORDER BY
+    highest_peak_elevation DESC,
+    longest_river_length DESC,
+    c.country_name
+LIMIT
+    5;
